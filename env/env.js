d288838dd57c26cee04d445bbe64982f5993c614
michael <michael@aaa.com>
2026-01-28 13:55:33 +0800

    -

--- env/env.js
+++ env/env.js
@@ -1,3 +1,7 @@
 window.$xai = {
-    apiBaseUrl: 'https://coordination-lauderdale-virtually-accepting.trycloudflare.com',
+    apiBaseUrl: 'https://api.100xai.io',
+    // apiBaseUrl: 'https://strip-medicines-intl-sen.trycloudflare.com',
+    // 与服务器时间差
+    secondOffset: 0,
+    contact: 'support@100xai.im',
 }
